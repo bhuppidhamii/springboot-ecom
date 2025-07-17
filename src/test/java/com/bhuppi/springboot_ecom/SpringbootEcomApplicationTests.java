24292c6664d381849a46c94d52706a49d940c42b
bhuppidhamii <dhamibhupendra3@gmail.com>
2025-07-17 09:51:12 +0530

    Project Setup

---
+++ src/test/java/com/bhuppi/springboot_ecom/SpringbootEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhuppi.springboot_ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
