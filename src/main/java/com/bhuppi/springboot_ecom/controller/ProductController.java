67532b5c0458dcfe9df81b9a9fc79a459577412d
bhuppidhamii <dhamibhupendra3@gmail.com>
2025-07-22 22:37:21 +0530

    Response Entity

--- src/main/java/com/bhuppi/springboot_ecom/controller/ProductController.java
+++ src/main/java/com/bhuppi/springboot_ecom/controller/ProductController.java
@@ -3,27 +3,35 @@ package com.bhuppi.springboot_ecom.controller;
 import com.bhuppi.springboot_ecom.model.Product;
 import com.bhuppi.springboot_ecom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
-    
+
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String greet() {
-        return "Hello, World!";
-    }
 
     @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    //    return only one product
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
